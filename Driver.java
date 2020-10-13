3c0cd13c27b1e3b3bd55f3ae9a64c56542a4108d
w4ng-cpu <y.a.wang@lancaster.ac.uk>
2020-10-13 22:06:11 +0100

    Added static sun and earth to solarSystem

--- Driver.java
+++ Driver.java
@@ -9,5 +9,7 @@ public class Driver
     */
     public static void main(String[] args) {
         SolarSystem solarSystem = new SolarSystem(300, 300);
+        solarSystem.drawSolarObject(0, 0, 10, "ORANGE");
+        solarSystem.drawSolarObjectAbout(30, 0, 4, "BLUE", 0, 0);
     }
 }
