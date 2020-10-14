25edffcf09f495d485bfc5d91eecaf37a430f6e0
w4ng-cpu <y.a.wang@lancaster.ac.uk>
2020-10-14 17:23:00 +0100

    Added time method so Earth can orbit

--- Driver.java
+++ Driver.java
@@ -5,11 +5,34 @@
 public class Driver
 {
     /**
+    * This is a method that keeps track of time according the 360 degrees of rotation
+    */
+    public static float timeCheck(float time)
+    {
+        if (time >= 360)
+        {
+            time = 0;
+        }
+        else
+        {
+            time += 1;
+        }
+        return time;
+    }
+
+    /**
     * This is the main method
+    * Instantiate SolarSystem class and call it's methods inside while loop
     */
     public static void main(String[] args) {
+        float time = 0;
         SolarSystem solarSystem = new SolarSystem(300, 300);
-        solarSystem.drawSolarObject(0, 0, 10, "ORANGE");
-        solarSystem.drawSolarObjectAbout(30, 0, 4, "BLUE", 0, 0);
+
+        while(true) {
+            time = timeCheck(time);
+            solarSystem.drawSolarObject(0, 0, 20, "ORANGE");
+            solarSystem.drawSolarObject(30, 3 * time, 6, "BLUE");
+            solarSystem.finishedDrawing();
+        }
     }
 }
